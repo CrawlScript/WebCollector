9b16cc6211e88a07c7f2ed407913bcc57ca8013e
hu <hujunxianligong@gmail.com>
2014-12-26 17:58:25 +0800

    1.50-beta

--- WebCollector/src/main/java/cn/edu/hfut/dmic/webcollector/generator/FSDbUpdater.java
+++ WebCollector/src/main/java/cn/edu/hfut/dmic/webcollector/generator/FSDbUpdater.java
@@ -29,11 +29,7 @@ import cn.edu.hfut.dmic.webcollector.util.FileUtils;
 import cn.edu.hfut.dmic.webcollector.util.LogUtils;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.CopyOption;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+
 
 
 /**
